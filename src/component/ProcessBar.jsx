46a7ab19593f0a40146d2970fa4e010320a50650
Amber-Song <jihongsongnz@gmail.com>
2021-08-13 16:20:17 +1200

    Fix process bar

--- src/component/ProcessBar.jsx
+++ src/component/ProcessBar.jsx
@@ -4,7 +4,10 @@ function ProcessBar({ questionNum, questionMax }) {
   return (
     <div className="processbar">
       <div className="processbar-whole"></div>
-      <div className="processbar-fill"></div>
+      <div
+        className="processbar-fill"
+        style={{ "--process": (questionNum / questionMax) * 100 + "%" }}
+      ></div>
     </div>
   );
 }
